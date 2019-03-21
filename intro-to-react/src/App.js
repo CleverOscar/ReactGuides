83fd2c2208d136183840f787ae22b263c4b807c6
Oscar Pacheco <Oscarpa88@yahoo.com>
2019-03-21 01:52:54 -0500

    Basic app generated, building a tic tac toe game with ReactJs

---
+++ intro-to-react/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Game from './components/Game'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h1>Tutorial: Intro to React </h1>
+            <Game />
+
+      </div>
+    );
+  }
+}
+
+export default App;
